40194f317095838cad674a026a60491a87dc39a1
illia-stack <illiashapshalov38@gmail.com>
2026-04-21 00:27:26 +0200

    Server für Deployment hinzugefügt

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-// server.js
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "build")));
+// Statische Dateien bereitstellen
+app.use(express.static(path.join(__dirname, 'build')));
 
-// Handle React routing, return all requests to React app
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+// Alle anderen Requests an React weiterleiten
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server läuft auf Port ${PORT}`);
 });
