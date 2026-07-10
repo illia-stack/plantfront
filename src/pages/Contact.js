6be3504bb4c2f5d49d94542c797cdf76ce560ead
illia-stack <illiashapshalov38@gmail.com>
2026-07-10 17:51:52 +0200

    syntax mistakes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import { AuthContext } = useContext(AuthContext);
+import { AuthContext } from "../context/AuthContext";
 import { API_BASE_URL } from "../config";
 
 function Contact() {
